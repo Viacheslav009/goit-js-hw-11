685d1b5ad65bd2a3077f88d14fdbb290ba11c2e5
Viacheslav009 <mstislavpushkov@gmail.com>
2023-04-03 15:30:26 +0300

    commit

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export function getImages(searchQuery, page, PAGINATION) {
-  const API_KEY = '34148451-f8d7bba54357f32edde6c57ad';
+export function getImages(searchQuery, page) {
+  const API_KEY = '34935030-b6fa315c9f10ffbefdfadca7a';
   const BASE_URL = 'https://pixabay.com/api/';
-  const URL = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PAGINATION}`;
+  const URL = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
   return axios.get(URL);
 }
